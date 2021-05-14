20088f010707568e20703be7f56373cb55540159
Leonel Barriga <leonel.barriga@galenoargentina.com.ar>
2021-05-13 21:13:40 -0300

    First commit

---
+++ scr/components/Loading.svelte
@@ -0,0 +1,3 @@
+<div class="Loading">
+    <p>Loading...</p>
+</div>
---
+++ scr/components/TimeLine.svelte
@@ -0,0 +1,22 @@
+<script>
+    import App from '../containers/App.svelte';
+import Card from './Card.svelte';
+    import Loading from './Loading.svelte';    
+    export let posts = [];
+</script>
+
+<style>
+    .TimeLine {
+      padding: 4em 0 0 0;
+    }
+</style>
+
+<div class="TimeLine">
+    <div class="TimeLine-container">
+        {#each posts as post}
+            <Card {...post} />
+        {:else}
+            <Loading />
+        {/each}
+    </div>
+</div>
